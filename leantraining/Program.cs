3db92e06da9a5e27842512daa2827da420abb6a8
timo.heiten <heitech.heiten@gmail.com>
2020-01-10 07:33:03 +0100

    added query to program.cs

--- leantraining/Program.cs
+++ leantraining/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using leantraining.DataAccess;
+using leantraining.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace leantraining
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var product = await GetLastProductAsync();
+
+            System.Console.WriteLine(product);
+            Console.ReadLine();
+        }
+
+        private static async Task<Product> GetLastProductAsync()
+        {
+            using (var session = new LeantrainingDbContext())
+            {
+                var date = DateTime.Now;
+                var first = await session.Set<Product>()
+                                   .Where(x => x.Start < date)
+                                   .OrderByDescending(x => x.Id)
+                                   .Include(x => x.Round)
+                                   .FirstOrDefaultAsync();
+                
+                return first;
+            }
         }
     }
 }
